168cae8e553a3a046c53b9aa10c7cb683d70ed92
Kai Bergmann <90706342+KaiBerg@users.noreply.github.com>
2022-12-07 14:47:31 +0100

    general improvements

--- src/Shared/Helper.cs
+++ src/Shared/Helper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +12,11 @@ namespace Games.Shared
     {
         public static int LastDigit(int number) => number % 10;
         public static int RemoveLastDigit(int number) => number / 10;
+        public static string GetRandomString()
+        {
+            var date = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+            var random = SHA512.Create().ComputeHash(date);
+            return BitConverter.ToString(random).Replace("-","");
+        }
     }
 }
